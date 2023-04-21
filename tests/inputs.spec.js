e10c9401d328180d5a76aad796ed35a38742275d
Akash K <akash.4balhara@gmail.com>
2023-04-21 23:38:11 +0530

    spin button

--- tests/inputs.spec.js
+++ tests/inputs.spec.js
@@ -7,15 +7,17 @@ test('Test Script to use a spin button', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/');
 
   // expect a title "to contain" a substring
-  await expect(page).toHaveTitle('Inputs');
+  await expect(page).toHaveTitle('The Internet');
 
   // enter the test name to be tested
-  const testName = 'Infinite Scroll';
+  const testName = 'Inputs';
 
   // opening the relevant page
   await page.getByRole('link', { name : testName, exact: true  }).click();
 
-  
+  // setting the input
+  await page.getByRole('spinbutton').fill('25');
+
 });
 
 
